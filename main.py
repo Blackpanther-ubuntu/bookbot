33371a31c71432e49a689836490faa3e9defc41f
Blackpanther-ubuntu <adam@artooro.com>
2025-11-09 16:28:31 -0500

    Added lots of code, trying to sort dictionarys

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from stats import num_words
+from stats import num_characters
+
+letters = {}
 
 def main():
     num_words("books/frankenstein.txt")
+    num_characters("books/frankenstein.txt", letters)
 
 main()
--- stats.py
+++ stats.py
@@ -1,6 +1,35 @@
+letter = {}
+
 def num_words(filepath):
     with open(filepath) as f:
         contents = f.read()
         words = contents.split()
         len_words = len(words)
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {filepath}...")
+        print("----------- Word Count ----------")
         print(f"Found {len_words} total words")
+
+def num_characters(filepath, dict):
+    with open(filepath) as f:
+        contents = f.read()
+        length = len(contents)
+        for i in range(0, length):
+            if contents[i].lower() in dict and contents[i].isalpha():
+                f = contents[i].lower()
+                dict[f"{f}"] = dict[f"{f}"] + 1
+            if contents[i].lower() not in dict and contents[i].isalpha():
+                t = contents[i].lower()
+                dict[f"{t}"] = 1
+        print(dict)
+
+def sorted_characters(dict):
+    dict2 = list(dict.items())
+    sorted_list = dict2.sort(reverse=True, key=dict)
+    print(sorted_list)
+
+def main():
+    num_characters("books/frankenstein.txt", letters)
+    #sorted_characters(letters)
+
+main()
